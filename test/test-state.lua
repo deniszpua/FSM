2b5bbcae31133bcf546dc6ca7692797ce52f0f15
denis <quux@ukr.net>
2015-10-21 22:09:33 +0300

    not executing inactive handlers methods added

--- test/test-state.lua
+++ test/test-state.lua
@@ -60,5 +60,60 @@ M.testOnUpdateHandlers = function ()
  return lunit_module.assertEquals(true, onEnterHandlersMock.isExecuted)
 end
 
+--------------------------------------------------------------------
+-- It should not call onExit and onUpdate handlers while executing
+-- enter method.
+M.testOnEnterOtherHandlersNotExecuted = function ()
+	before()
+	
+	local handlersMock = {
+	 isOnExitExecuted = false, 
+	 isOnUpdateExecuted = false
+ }
+	state_instance.addHandler('onExit', function () handlersMock.isOnExitExecuted = true end)
+	state_instance.addHandler('onUpdate', function () handlersMock.isOnUpdateExecuted = true end)
+	state_instance.enter()
+	
+	return lunit_module.assertEquals(false, handlersMock.isOnExitExecuted or handlersMock.isOnUpdateExecuted)
+	
+end
+
+--------------------------------------------------------------------
+-- It should not call onEnter and onUpdate handlers while executing
+-- exit method.
+M.testOnExitOtherHandlersNotExecuted = function ()
+	before()
+	
+	local handlersMock = {
+	 isOnEnterExecuted = false, 
+	 isOnUpdateExecuted = false
+ }
+	state_instance.addHandler('onEnter', function () handlersMock.isOnEnterExecuted = true end)
+	state_instance.addHandler('onUpdate', function () handlersMock.isOnUpdateExecuted = true end)
+	state_instance.exit()
+	
+	return lunit_module.assertEquals(false, handlersMock.isOnEnterExecuted or handlersMock.isOnUpdateExecuted)
+	
+end
+
+--------------------------------------------------------------------
+-- It should not call onEnter and onExit handlers while executing
+-- update method.
+M.testOnUpdateOtherHandlersNotExecuted = function ()
+	before()
+	
+	local handlersMock = {
+	 isOnEnterExecuted = false, 
+	 isOnExitExecuted = false
+ }
+	state_instance.addHandler('onEnter', function () handlersMock.isOnEnterExecuted = true end)
+	state_instance.addHandler('onExit', function () handlersMock.isOnExitExecuted = true end)
+	state_instance.update()
+	
+	return lunit_module.assertEquals(false, handlersMock.isOnEnterExecuted or handlersMock.isOnExitExecuted)
+	
+end
+
+
 
 return M
