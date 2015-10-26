ec0641b4fcff7897e5ec81315183dfbf3824340f
denis <quux@ukr.net>
2015-10-26 22:39:45 +0200

    junction condition recognizer added

--- src/jsonStateLoader.lua
+++ src/jsonStateLoader.lua
@@ -6,8 +6,9 @@ local M = {}
 local json = require("dkjson")
 local path = require("path")
 
-local io = io 
+local io = io
 local assert = assert
+local load = load
 local print = print
 local pairs = pairs
 local error = error
@@ -36,61 +37,72 @@ local function objToString(obj)
 end
 
 function main()
-  -- sample string for unit test
-  local jsonString =
-    [[{"FSM":
-    {"Name":"cvetok5_zone_controller", 
-      "States":[
-        {"Name":"idle", 
-          "Junctions": [
-            {"Condition":"event.cvetok5_zone_touched and not GetSprite('cvetok5').is_active",
-            "State":"cvetok5"
-              },
-            {"Condition":"event.cvetok5_zone_touched and GetSprite('cvetok5').is_active",
-            "State":"cvetok5_1"
-              }
-            ]
-          },
-        {"Name":"cvetok5",
-          "Animation":"p1_cvetok5_1",
-          "NextState":"idle",
-          "OnEnter":"PlaySound('p1_cvetok5_1')DeactivateSprites('jagoda5')",
-          "OnExit":"ActivateSprites('cvetok5', 'list5_1', 'list5_2')"
-          },
-        {"Name":"cvetok5_1",
-          "Animation":"p1_cvetok5_2",
-          "NextState":"idle",
-          "OnEnter":"PlaySound('p1_cvetok5_2')DeactivateSprites('cvetok5', 'list5_1', 'list5_2')",
-          "OnExit":"ActivateSprites('jagoda5')"
-          }
-        ],
-      "StartState":"idle"
-      }
-  }]]
 
-    local loadedObject = M.loadJsonData(M.loadStringFromFile('/home/dev-user/Desktop/sample.json'))
-    print(objToString(loadedObject))
+  local loadedObject = M.loadJsonData(M.loadStringFromFile('/home/dev-user/Desktop/sample.json'))
+  print(objToString(loadedObject))
 
 end
 
-
+---
+-- Creates object that represents entity, described by specified jsonString.
+--
+-- Uses dkjson library, that should be on project's build path.
+-- @param #string jsonString valid json string representation of object
+-- @return #table table, which structure was described by json and with specified keys
+-- and values
 function M.loadJsonData(jsonString)
   local obj, pos, err = json.decode(jsonString)
   if err then
     error("Cannot parse json")
   else
+    if obj.fsm ~= nil then obj = obj.fsm end
     return obj
   end
 
 end
 
+
+---
+-- Loads string from file.
+--
+-- Current implementation is case-insensitive. "~" sign in path is not supported
+-- (standart io library limitation).
 function M.loadStringFromFile(pathToJsonFile)
-	local file =  assert(io.open(pathToJsonFile, "r"))
-	local str = file:read("*a")
-	file:close()
-	return str
+  local file =  assert(io.open(pathToJsonFile, "r"))
+  local str = file:read("*a")
+  file:close()
+  return string.lower(str)
 end
 
+---
+-- Replaces conditions code with their predicate functions.
+--
+-- Condition should be specified
+-- @return #table with conditional predicates instead of their code snippets
+function M.recognizeConditions(fsm)
+
+  -- traverse fsm -> states [foreach state -> junctions [foreach junction -> condition => func]
+  local states = fsm.states
+
+  if states then
+    for _, state in pairs(states) do
+      if state.junctions then
+        for _, junction in pairs(state.junctions) do
+          local conditionCode = load(junction.condition)
+          if not conditionCode then
+            error(string.format("Condition for state: %s not recognized", state.name))
+          else
+            junction.condition = conditionCode
+          end
+
+        end
+      end
+    end
+  end
+
+  return fsm
+
+end
 
 
 main()
